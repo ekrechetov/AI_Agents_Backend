cc38adbb157e9c4eda087185f61cccd9478fed04
ekrechetov <yevhenii.krechetov@gmail.com>
2026-03-04 02:23:52 +0100

    fix not found

--- src/routes/aiChatRoutes.ts
+++ src/routes/aiChatRoutes.ts
@@ -5,8 +5,8 @@ const router = Router()
 
 router.post('/api/chat', handleChatStream)
 
-router.get('/', function (req, res) {
-	console.log('Received request to /, Hello !!!!')
+router.get('/api/get', function (req, res) {
+	console.log('Received request to /, Hello !!!!', req.body)
 	res.send('Hello World!!!!')
 })
 
